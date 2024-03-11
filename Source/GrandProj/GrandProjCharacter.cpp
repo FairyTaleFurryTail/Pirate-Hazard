333a29c3b45aa0fb3d83f38e1d7f1041b4444035
vacuname <836610245@qq.com>
2024-03-11 17:01:30 +0800

    C++动态切换动画 设置了所有动画的RootMotion

--- Source/GrandProj/GrandProjCharacter.cpp
+++ Source/GrandProj/GrandProjCharacter.cpp
@@ -49,7 +49,6 @@ AGrandProjCharacter::AGrandProjCharacter()
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
 	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
-
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
 }
@@ -68,6 +67,8 @@ void AGrandProjCharacter::BeginPlay()
 			Subsystem->AddMappingContext(DefaultMappingContext, 0);
 		}
 	}
+
+	GetMesh()->LinkAnimClassLayers(UnArmLayer);
 }
 
 //////////////////////////////////////////////////////////////////////////
--- Source/GrandProj/GrandProjCharacter.h
+++ Source/GrandProj/GrandProjCharacter.h
@@ -44,6 +44,9 @@ class AGrandProjCharacter : public ACharacter
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* LookAction;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<UAnimInstance>UnArmLayer;
+
 public:
 	AGrandProjCharacter();
 	
